9605a1c032e210b6620ca2bcb5d0c6ed8adc127f
최은정 <choeeunjeong@MacBook-Pro.local>
2021-12-06 00:40:24 +0900

    book_py_01.py

--- py/book_py_01.py
+++ py/book_py_01.py
@@ -2,6 +2,22 @@
 jack = 0  #변수의 초기화, 값설정
 i, k, j = 10, 11, 12
 jack = i+k
-print(jack)
+print(jack) #10+11의 값을 프린트
+jack = j-i
+print(jack) #12-10의 값을 프린트
+jack = jack * 2
+print(jack) #(12-10)*2의 값을 프린트
+jack = k/jack
+print(jack) #11를 (12-10)*2로 나누어 남은 나머지의 값을 프린트
+jack = j//jack 
+print(jack) #12를 3으로 나누어 남은 몫을 프린트
+
+print("what is your age?")
+textvalue = int(input("enter the age /10: "))
+numvalue = int(input("enter the number//10 : "))
+jack = (numvalue*10)+textvalue
+print("my age is", jack)
+
+
 
 
