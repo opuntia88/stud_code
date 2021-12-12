2c6557cf8b8421347e309a3800dd10867a683aaf
최은정 <choeeunjeong@MacBook-Pro.local>
2021-12-13 04:58:53 +0900

    book_py_01_test.py

--- py/book_py_01.py
+++ py/book_py_01.py
@@ -12,11 +12,11 @@ print(jack) #11를 (12-10)*2로 나누어 남은 나머지의 값을 프린트
 jack = j//jack 
 print(jack) #12를 3으로 나누어 남은 몫을 프린트
 
-print("what is your age?")
-textvalue = int(input("enter the age /10: "))
-numvalue = int(input("enter the number//10 : "))
-jack = (numvalue*10)+textvalue
-print("my age is", jack)
+print("what is your age?") # 너의 나이는 얼마고
+textvalue = int(input("enter the age /10: ")) #내 나이의 일의 자리 숫자 입력
+numvalue = int(input("enter the number//10 : ")) #내 나이의 십의 자리 숫자 입력 
+jack = (numvalue*10)+textvalue #내 나이 계산
+print("my age is", jack) #한걸 출력
 
 
 
