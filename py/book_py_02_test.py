f72bae75d421d5d43df053cd2ab0508aa4718936
최은정 <choeeunjeong@MacBook-Pro.local>
2022-01-03 18:59:49 +0900

    book_py_02_test.py

--- py/book_py_02_test.py
+++ py/book_py_02_test.py
@@ -29,3 +29,13 @@ else:
     print("i don't like that color, i prefer red.")
 
 #16.날씨가 비가오는지 묻고, 그렇다면 바람이 부는지도 확인하여 사용자에게 우산을 챙기고 조심여부를 안내.
+in1=input("today's weather is rainy?(y/n):")
+in2=input("today's weather is windy?(y/n):")
+an1,an2=str.lower(in1,in2)
+if an1 =="yes":#비가 올경우
+    if an2 =="yes":#비오는데 바람까지 불 경우
+        print("it is too windy to use an umbrella")
+    else:
+        print("Take an umbrella")
+else:
+    print("Have a good day!")
