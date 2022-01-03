ff7d0b1c7ac08ba91a26c7ac181e4605a84f9bbc
최은정 <choeeunjeong@MacBook-Pro.local>
2022-01-03 18:44:18 +0900

    book_py_02_test.py

---
+++ py/book_py_02_test.py
@@ -0,0 +1,31 @@
+#12 두 숫자를 입력받아, 큰 숫자부터 출력하라
+num1,num2=input("enter the two numbers(should be different, seperate " " )").split()
+numA,numB=int(num1,num2)
+if numA>numB:
+    print("%d > %d" %(numA,numB))
+else:
+    print("%d > %d" %(numB,numA))
+
+#13.사용자에게 20보다 작은 수를 입력받아, 입력받은 값이 20보다 같거나 크면"too high", 작으면 "thank you"출력
+num3=input("enter the a number is smaller than 20: ")
+if num3 == 20 or num3 >20:
+    print("too high~")
+else:
+    print("Thank you!")
+
+#14. 사용자에게 10과 20사이의 수를 입력받아 사잇값일 경우 감사를 표하고 아닐경우 오류 표시하기
+num4=input("enter the a number between 20 to 10: ")
+if num4 <= 20 and num4 >= 10:
+    print("Thank you")
+else:
+    print("incorrect number")
+
+#15 사용자에게 좋아하는 색상을 입력받고, red를 입력받을 경우 "i love red,too" 출력하거, 아닐경우 레드선호의 답하기
+coL=input("what's your favorite colot?:")
+coloR=str.lower(coL) #문자를 소문자로 바꾸는 함수
+if coloR=="red":
+    print("i like red too~")
+else:
+    print("i don't like that color, i prefer red.")
+
+#16.날씨가 비가오는지 묻고, 그렇다면 바람이 부는지도 확인하여 사용자에게 우산을 챙기고 조심여부를 안내.
