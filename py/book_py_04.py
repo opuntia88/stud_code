2a1f526c81866a11cb3bd877081546c69c265337
opuntia88 <tylenol0000@gmail.com>
2022-01-06 23:58:45 +0900

    book_py_04.py

---
+++ py/book_py_04.py
@@ -0,0 +1,11 @@
+import math
+#4.수학함수
+#수학함수의 경우, 데이터가 정수또는 부동소수점일경우에만 사용가능.
+
+nm1=input("enter float number:")
+print(round(nm1,2)) # ► 숫자를 반올림하여 소수점 둘째자리까지 표현한다.
+
+nm2=input("enter a neutral number:")
+print(nm2**3) #  ► A**B는 a의 b거듭제곱이다!
+math.sqrt(nm2) # ► 숫자의 제곱근을 구함, 사용시 import math를 반드시 추가해야함
+
