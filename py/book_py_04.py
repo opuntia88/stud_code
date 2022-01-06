f5daecff8c5f7883fcc85d051e958fde6f62af4d
opuntia88 <tylenol0000@gmail.com>
2022-01-07 00:08:34 +0900

    book_py_04(1).py

--- py/book_py_04.py
+++ py/book_py_04.py
@@ -9,3 +9,13 @@ nm2=input("enter a neutral number:")
 print(nm2**3) #  ► A**B는 a의 b거듭제곱이다!
 math.sqrt(nm2) # ► 숫자의 제곱근을 구함, 사용시 import math를 반드시 추가해야함
 
+nm3=float(input("enter number:")) 
+# ► 정수부분과 소수부분으로 나누어지는 소수점을 가진 수를 받는다.
+
+m4=math.pi # ► 소수 15자리까지 파이의 값을 반환, 사용시 import math를 반드시 추가해야함
+print(m4)
+
+m5, m6=int(input("enter the two numbers(must a>b):"))
+m7=m5//m6 # 몫 연산
+m8=m5%m6 # 나머지 연산
+
