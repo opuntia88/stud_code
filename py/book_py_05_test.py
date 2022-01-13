b7cdf0e65685f40a45653d46ee77a3b2aa1fa0ba
opuntia88 <tylenol0000@gmail.com>
2022-01-13 16:24:45 +0900

    book_py_05_test(7).py

--- py/book_py_05_test.py
+++ py/book_py_05_test.py
@@ -35,7 +35,7 @@ for i in range(0,51-nm4):#입력한 숫자까지 출력을 할 경우, 끝값에
     print(50-i)
 
 #41. 이름과 숫자를 입력하도록 요청하여 입력한 숫자가 10미만이면 이름을 출력, 이상이면 too high를 출력.
-n4,nm5=input("enter the number and name(seperate to " ":").split()
+n4,nm5=input("enter the number and name(seperate to " "):").split()
 n4=int(n4)
 #nm5=str(nm5)
 if n4<=10:
@@ -45,3 +45,39 @@ else:
     print("Too High~")
 
 #42.total=0, 5번 숫자입력을 요청하여 입력받은 숫자값을 더할지 여부를 묻고, 5번 물어본후 최후값을 출력.
+total=0
+for i in range(0,5):#똑같은 형태의 질문이므로 for 문으로 반복하여 묻는다
+    n5=int(input("enter the number:"))
+    ans=input("Do you want to add the number?(y/n):")
+    ans.lower()#소문자로 변형
+    if ans == "yes":#yes일 경우
+        total=total+n5
+    elif ans == "no":#no일 경우
+        total=total
+    else:
+        print("incorrect")
+print("total=%d"%total)
+
+#43. 사용자에게 원하는 카운트 방향을 묻고, 시작할 숫자또는 20미만의 숫자로의 다운을 출력
+d=input("up?or down?:")
+d.lower()
+if d=="up":#up을 입력했을 경우, 
+    n6=int(input("enter the number:"))
+    for j in range(0,n6+1):#
+        print(j)
+elif d=="down":
+    n6=int(input("enter the number under 20:"))
+    for i in range(0,n6+1):
+        print(n6-i)
+else:
+    print("l don't understand.")
+
+#44. 파티에 몇명을 초대하고싶은지 묻고, 10미만이면 이름을 묻고 "[이름]has been invited"을 입력한 숫자만큼 반복. 10 이상이면 "too many people을 출력"
+n7=int(input("How many do you invite the people?:"))
+if n7<10:
+    nm6=input("enter your name:")
+    for i in range(0, n7):
+        print("%s has been invited")
+else:
+    print("Too much people~")
+
