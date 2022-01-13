18221ac82932345dd412b75326a535645ddc6c89
opuntia88 <tylenol0000@gmail.com>
2022-01-13 16:38:19 +0900

    book_py_05_test(10).py

--- py/book_py_05_test.py
+++ py/book_py_05_test.py
@@ -77,8 +77,8 @@ else:
 #44. 파티에 몇명을 초대하고싶은지 묻고, 10미만이면 이름을 묻고 "[이름]has been invited"을 입력한 숫자만큼 반복. 10 이상이면 "too many people을 출력"
 n7=int(input("How many do you invite the people?:"))
 if n7<10:
-    nm6=input("enter your name:")
     for i in range(0, n7):
+        nm6=input("enter your name:")
         print("%s has been invited" %nm6)
 else:
     print("Too much people~")
