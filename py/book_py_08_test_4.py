a43d01267a927b4970c58593555525cab21fdb5e
opuntia88 <tylenol0000@gmail.com>
2022-01-24 04:22:34 +0900

    book_py_08_test_4.py

--- py/book_py_08_test_4.py
+++ py/book_py_08_test_4.py
@@ -1,7 +1,7 @@
 import turtle
 
 scr=turtle.Screen()
-scr.bgcolor("yellow")
+scr.bgcolor("white")
 turtle.shape("turtle")
 turtle.shapesize(1)
 turtle.pensize(2)
@@ -28,19 +28,25 @@ turtle.pensize(2)
 
 
 #63. 서로 붙어있지않는 세게의 정 삼각형을 그려라, 세개의 정삼각형은 다른 색으로 채워져있따
-turtle.left(90)
-turtle.forward(100)
+#turtle.penup()
+#turtle.left(180)
+#turtle.forward(100)
+#turtle.pendown()
 for i in range(0,3):
-    if i==0:
+    turtle.penup()#펜을 띄운다.
+    if i==0:#각 도형의 색을 다르게 한다.
         turtle.color("black","red")
     elif i==1:
         turtle.color("black","orange")
     elif i==2:
         turtle.color("black","yellow")
-    turtle.right(180)
+    turtle.right(90)
     turtle.forward(100)
-    turtle.begin_fill()
+    turtle.pendown()#도형을 그릴 점부터 선을 내린다.
+    turtle.begin_fill()#그릴 도형에만 색상을 씌운다.
     for j in range(0,3):
         turtle.forward(100)
         turtle.right(120)
+    turtle.end_fill()
+    
 turtle.exitonclick()
