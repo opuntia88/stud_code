a9ba97dc3e28fcdba42978741c6f807bd66ca67e
opuntia88 <tylenol0000@gmail.com>
2022-02-13 19:31:19 +0900

    book_py_11_test(2).py

--- py/book_py_11_test.py
+++ py/book_py_11_test.py
@@ -21,3 +21,33 @@ for i in range(0,5):
 
 #90.사용자에게 숫자를 입력하라고 요청한다. 10에서 20사이일 경우 배열에 저장, 다른값이면 outside the range출력
 #   다섯개가 입력이 되면 thank you 출력하여 배열항목에 하나씩 출력.
+inp03=array('i',[])
+n=0
+c=0#invalid literal for int() with base 10: '' -> c가 정수인지 값이 지정되지 않았으므로 28열에서 오류가 발생.
+while n<5:
+    c=int(input("enter the number between 10 to 20:"))
+    if c<=20 and c>=10:
+        inp03.append(c)
+        n=n+1
+    else:#10<=c<=20에 포함되지 않을 경우.
+        print("outside the range")
+
+for i in range(0,5):#하나의 항목씩 출력.
+    print(inp03[i])
+
+
+#91. 다섯개의 숫자를 담고있는 배열을 생성. 사용자ㅔ게 배열 전체를 출력 후 하나를 고르라고하여 리스트에 몇개가 있는지 출력.
+inp04=array('i',[45,12,9,12,45])
+n=0
+print("the list is",inp04)
+d=int(input("choose one number in array:"))
+for i in inp04:
+    if i == d:
+        n=n+1#어레이 안에 있는만큼 더하기
+
+print(d,"is ",n,"in array")
+
+#92.
+
+
+        
