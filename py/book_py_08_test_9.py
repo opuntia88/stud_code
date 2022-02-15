d3912d83cb77674054325e708f34ceda52d1776b
opuntia88 <tylenol0000@gmail.com>
2022-02-15 21:33:15 +0900

    book_py_08_test_9(10).py

--- py/book_py_08_test_9.py
+++ py/book_py_08_test_9.py
@@ -9,7 +9,7 @@ turtle.pensize(2)
 
 #67.랜덤함수를 이용해서 각 선의 길이, 개수, 회전 각도를 정하라.
 turtle.penup()#초기값 설정을 위한 이동의 선이 보이지않도록 펜을 띄움
-n_len=random.randint(10,100)#선의 길이=>
+n_len=random.randint(12,100)#선의 길이=>
 n_num=random.randint(4,20)#선의 갯수->내부 도형의 
 n_ang=random.randint(20,180)#회전각도
 turtle.color("green","")#속을 색칠하지않은 도형 윤곽선 채색
--- py/book_py_12.py
+++ py/book_py_12.py
@@ -3,10 +3,21 @@
 # 2차원 리스트가 훨씬 더 일반적이다.
 #2차원 리스트의 코드는
 grades=[[14,23,54],[32,22,45],[23,32,33]]
-#for i in grades:
+for i in grades:
+    print(i)# 이렇게 하면 각 row에 해당되는 column이 출력됨
+for i in grades[1]:#이렇게하면 row[1]에 해당되는 
+    print(i)
+
 #    for j in grades[i]:
 #        print(grades[i][j])
 #에러 TypeError: list indices must be integers or slices, not list -> 인덱싱은 정수나 슬라이딩 부호[:]로 하는 것
-#표준 파이썬 컬럼 인덱스 번호를 사용하고싶지않을 경우 다음과 같이 딕셔너리로 사용
+
+#표준 파이썬 컬럼 인덱스 번호를 사용하고싶지않을 경우 다음과 같이 딕셔너리로 사용 ->[{}]->리스트 내부에 각 요소를 딕셔너리화
 grade=[{"ma":14,"en":23,"fr":54},{"ma":32,"en":22,"fr":45},{"ma":23,"en":32,"fr":33}]
 print(grade[0]["en"])
+
+#행의 인덱스도 추가가 가능하다.-> {{}} ->r과 c모두 딕셔너리화
+grade={"susan":{"ma":14,"en":23,"fr":54},"peter":{"ma":32,"en":22,"fr":45},"jack":{"ma":23,"en":32,"fr":33}}
+print(grade["susan"],["en"])
+simply_array=[[2,5,8],[3,7,4],[1,6,9]]
+
