5588681aa8bb56cb60f51d6de50b5783a2b7581d
opuntia88 <tylenol0000@gmail.com>
2022-02-16 02:24:54 +0900

    book_py_12(6).py

--- py/book_py_12.py
+++ py/book_py_12.py
@@ -19,5 +19,26 @@ print(grade[0]["en"])
 #행의 인덱스도 추가가 가능하다.-> {{}} ->r과 c모두 딕셔너리화
 grade={"susan":{"ma":14,"en":23,"fr":54},"peter":{"ma":32,"en":22,"fr":45},"jack":{"ma":23,"en":32,"fr":33}}
 print(grade["susan"],["en"])
-simply_array=[[2,5,8],[3,7,4],[1,6,9]]
 
+simple_array=[[2,5,8],[3,7,4],[1,6,9]]
+print("simple_array",simple_array) #2차원어레이에있는 모든 데이터를 출력.
+print("simple_array[1]",simple_array[1])
+simple_array[1][2]=3
+print("simple_array[1][2]",simple_array[1][2])
+
+data_set={"a":{"x":54,"y":82,"z":91},"b":{"x":75,"y":29,"z":80}} #2차원 딕셔너리 생성
+print(data_set["a"])# a행의 모든 열에 포함된 데이터를 출력.
+print(data_set["a"]["z"])#b행의 z열에 포함되는 데이터를 출력
+for i in data_set:
+    print(data_set[i]["y"])#각 행에있는 y열의 데이터를 출력.
+data_set["b"]["y"]=56
+print(data_set)
+
+clas={}#초반에 딕셔너리임을 정의해야 오류가 안생김
+clas["jack"]={"math":45 ,"english":30}
+print(clas)
+for name in clas:
+    print(("jack"),clas["jack"]["english"])#이름을 출력하고 english에 해당되는 값을 출력
+
+del clas["jack"]["english"]#del을 사용해서 clas리스트의 '"name"열을 삭제
+print(clas)
