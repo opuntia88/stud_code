c8a3ef26fdf04c7ada69136f58a4f6435e6dacc1
opuntia88 <tylenol0000@gmail.com>
2022-03-09 00:17:10 +0900

    book_py_12_test(7).py

--- py/book_py_12_test.py
+++ py/book_py_12_test.py
@@ -26,13 +26,22 @@ for i in l1:
     print(i)
 
 #99.요청받은위치의 행을 출력하여 그중 열의 위치를 입력받아 출력한다. 값을 변경의 원함여부를 요청해 그럴경우 변경해 행을 출력,
-r3=int(input("choose the row number between 0 to 3:"))
+r3=int(input("choose the row number between 0 to 3:"))#원하는 행의 위치를 선택
 print(l1[r3])
-c3=int(input("choose the column number between 0 to 2:"))
+c3=int(input("choose the column number between 0 to 2:"))#원하는 열의 위치를 선택
 print(l1[r3][c3])
-ans3=input("do you want to change the data?(y/n):")
-if ans3=="y":
+ans3=input("do you want to change the data?(y/n):")#이 위치의 값을 변경할 건지 선택
+if ans3=="y":#바꾸길 원할 경우
     cng=int(input("enter the number:"))
     l1[r3][c3]=cng
-    print(l1[r3])
+    print(l1)
+else:
+    print("you entered wrong word")
+
+#100.2차원 딕셔너리를 사용하여 각 사람이 서로 다른 지역에서 만든 매출을 보여주는 다음의 데이터를 생성하다.
+sales={"John":{"n":3056, "s":8463, "e":8441, "w":2694},"Tom":{"n":4832, "s":6786, "e":4737, "w":3612},
+        "Anne":{"n":5239, "s":4802, "e":5820, "w":1859},"Fiona":{"n":3904, "s":3645, "e":8821, "w":2451}}
+
+
+
 
